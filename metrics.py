5fd1dc6639468dfa141783d8ab59282ed0ed1f79
ZengHao-Kendo <45357384+ZengHao-Kendo@users.noreply.github.com>
2018-12-16 22:25:43 +0800

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -8,16 +8,67 @@ for dataset in DATA_FILE_PATH:
     query_set: Set[str] = set(map(lambda data_tuple: data_tuple[0],
                                   query_doc_label_generator(dataset = dataset)))
     QUERY_NUM[dataset] = len(query_set)
+####################################################
+#Unknown,,,
+QUERY_CORRECT_NUM: Dict[str, int]
+FINE_LABLE_DICT: Dict[str,list(int)]#true anwser label of test dataset
 
+####################################################
+def get_map(data_set_name):
+	itor = query_doc_label_generator(data_set_name)
+    total_sum = 0
+    cnt = 0 
+    for content in itor:
+    	cnt += 1
+    	cur_query = content[0]
+    	doc_list = content[1]
+    	label_list = content[2]
+    	if data_set_name == 'tset':
+	    	fine_label = FINE_LABLE_DICT[cur_query]
+	    else:
+	    	fine_label = label_list
+    	correct_num = sum(fine_label)
+    	denominator = min(QUERY_NUM[cur_query],correct_num)
+    	if denominator == 0:
+    		continue
+    	inner_sum=0
+    	for k in range(0,len(doc_list)):
+    		temp = label_list[k]*fine_label[k]
+    		inner_sum += temp
+    	total_sum + = inner_sum
+    return total_sum/cnt
+
+def get_mrr(data_set_name):
+	itor = query_doc_label_generator(data_set_name)
+    total = 0
+    cnt = 0 
+    for content in itor:
+    	cnt += 1
+    	cur_query = content[0]
+    	doc_list = content[1]
+    	label_list = content[2]
+    	if data_set_name == 'tset':
+	    	fine_label = FINE_LABLE_DICT[cur_query]
+	    else:
+	    	fine_label = label_list
+    	merged_list = list(map(lambda x,y:[x,y],label_list,fine_label))
+    	merged_list.sort(reverse=True)
+    	k=0
+    	for i in merged_list:
+    		k+=1
+    		if i[1]==1:
+    			total += 1/k
+    			break
+    return total/cnt
 
 def mean_average_precision():
     # TODO: implement mean_average_precision
-    raise NotImplementedError
+    return get_map('test')
 
 
 def mean_reciprocal_rank():
     # TODO: implement mean_average_precision
-    raise NotImplementedError
+    return get_mrr('test')
 
 
 # add aliases
