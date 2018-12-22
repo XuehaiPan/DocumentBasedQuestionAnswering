8ee4effa0131a719443f4d23473dd74b267d7ea2
AnTuo <30630297+AnTuo1998@users.noreply.github.com>
2018-12-22 18:26:26 +0800

    fix little bugs

--- analysis.py
+++ analysis.py
@@ -4,18 +4,14 @@ import numpy as np
 from jieba import Tokenizer
 from config import POSITIVE, NEGATIVE, \
     VEC_SIZE, MAX_QUERY_WC, MAX_DOC_WC, BIN_NUM, \
-    DATA_FILE_PATH, DICTIONARY_PATH, FIGURE_DIR,BATCH_SIZE,MODEL_DIR
+    DATA_DIR, DICTIONARY_PATH, FIGURE_DIR,BATCH_SIZE,MODEL_DIR
 from keras.models import load_model
 from dataset import DataSequence
 
-model_list=['epoch01_acc0.7526.h5','epoch02_acc0.8323.h5','epoch03_acc0.8398.h5',
-			,'epoch04_acc0.7892.h5','epoch05_acc0.8858.h5','epoch06_acc0.7938.h5',
-			'epoch07_acc0.7673.h5','epoch08_acc0.8677.h5','epoch09_acc0.8349.h5',
-			'epoch10_acc0.7887.h5']
 
 def predict_test(idx:int,model,x):
 	strtemp='test_score'+str(idx)+'.txt'
-	f_ans = open(os.path.join(DATA_FILE_PATH, strtemp)+,'w')
+	f_ans = open(os.path.join(DATA_DIR, strtemp),'w')
 	predictions: np.ndarray = model.predict_generator(x, max_q_size=10, workers=1, pickle_safe=False, verbose=0)
 	print(predictions.shape)
 	for i in predictions:
@@ -25,7 +21,7 @@ def predict_test(idx:int,model,x):
 
 def predict_valid(idx:int,model,x):
 	strtemp='valid_score'+str(idx)+'.txt'
-	f_ans = open(os.path.join(DATA_FILE_PATH, strtemp)+,'w')
+	f_ans = open(os.path.join(DATA_DIR, strtemp),'w')
 	predictions: np.ndarray = model.predict_generator(x, max_q_size=10, workers=1, pickle_safe=False, verbose=0)
 	for i in predictions:
 		for j in i:
@@ -45,11 +41,14 @@ print('make valid...')
 x_valid = DataSequence(dataset = 'validation', batch_size = BATCH_SIZE, data_augmentation = False, return_target = True)
 # x_ = DataSequence(dataset = 'validation', batch_size = BATCH_SIZE, data_augmentation = False, return_target = False)
 
+model_list = [x for x in os.listdir(MODEL_DIR) if 'h5' in x]
+print(model_list)  
+
 for file_path in model_list:
 	model = load_model(os.path.join(MODEL_DIR, file_path))
 	print('for load model '+str(cnt))	
 	print('test predict...')
 	predict_test(cnt,model,x_test)
 	print('valid predict.')
-	predict_valid(cnt,model,x_test)
+	predict_valid(cnt,model,x_valid)
 	cnt+=1
