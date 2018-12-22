a7c1514a4a9d8391e69b6750fc617dcba6594b5d
XuehaiPan <XuehaiPan@pku.edu.cn>
2018-12-22 22:55:34 +0800

    refactor analysis.py

--- analysis.py
+++ analysis.py
@@ -1,54 +1,45 @@
 import os
-from typing import List, Tuple, Dict, Iterator, Union
+from typing import List, Set
 import numpy as np
-from jieba import Tokenizer
-from config import POSITIVE, NEGATIVE, \
-    VEC_SIZE, MAX_QUERY_WC, MAX_DOC_WC, BIN_NUM, \
-    DATA_DIR, DICTIONARY_PATH, FIGURE_DIR,BATCH_SIZE,MODEL_DIR
-from keras.models import load_model
+import keras
+from config import DATA_DIR, BATCH_SIZE, WORKERS
+from model import get_model_paths, build_network
 from dataset import DataSequence
 
 
-def predict_test(idx:int,model,x):
-	strtemp='test_score'+str(idx)+'.txt'
-	f_ans = open(os.path.join(DATA_DIR, strtemp),'w')
-	predictions: np.ndarray = model.predict_generator(x, max_q_size=10, workers=1, pickle_safe=False, verbose=0)
-	print(predictions.shape)
-	for i in predictions:
-		for j in i:
-			f_ans.write(str(j)+'\n')
-	f_ans.close()
+def save_predictions(predictions: np.ndarray, filename: str) -> None:
+    with open(file = filename, mode = 'w', encoding = 'UTF-8') as file:
+        for score in predictions.flatten():
+            file.write(f'{score}\n')
 
-def predict_valid(idx:int,model,x):
-	strtemp='valid_score'+str(idx)+'.txt'
-	f_ans = open(os.path.join(DATA_DIR, strtemp),'w')
-	predictions: np.ndarray = model.predict_generator(x, max_q_size=10, workers=1, pickle_safe=False, verbose=0)
-	for i in predictions:
-		for j in i:
-			f_ans.write(str(j)+'\n')
-	f_ans.close()
 
-def evaluate_valid(idx:int,model,x,f_loss,f_acc):
-	evaluations: np.ndarray = model.evaluate_generator(x, steps=1, max_q_size=10, workers=1, pickle_safe=False)
-	for i in evaluations:
-		f_loss.write(str(i[0])+'\n')		
-		f_acc.write(str(i[1])+'\n')
+def predict() -> None:
+    validation_data = DataSequence(dataset = 'validation', batch_size = BATCH_SIZE,
+                                   data_augmentation = False, return_target = False)
+    test_data = DataSequence(dataset = 'test', batch_size = BATCH_SIZE,
+                             data_augmentation = False, return_target = False)
+    
+    model_paths: List[str] = get_model_paths(sort_by = 'val_acc', reverse = True)
+    top5_model_paths: Set[str] = set(model_paths[:5])
+    model_paths: List[str] = get_model_paths(sort_by = 'epoch', reverse = False)
+    
+    for epoch, model_path in enumerate(model_paths, start = 1):
+        model: keras.Model = build_network(model_path = model_path)
+        validation_file: str = os.path.join(DATA_DIR, f'validation_score_epoch{epoch}.txt')
+        test_file: str = os.path.join(DATA_DIR, f'test_score_epoch{epoch}.txt')
+        if not os.path.exists(validation_file):
+            validation_predictions: np.ndarray = model.predict_generator(generator = validation_data, verbose = 1,
+                                                                         workers = WORKERS, use_multiprocessing = True)
+            save_predictions(predictions = validation_predictions, filename = validation_file)
+        if not os.path.exists(test_file) and model_path in top5_model_paths:
+            test_predictions: np.ndarray = model.predict_generator(generator = test_data, verbose = 1,
+                                                                   workers = WORKERS, use_multiprocessing = True)
+            save_predictions(predictions = test_predictions, filename = test_file)
 
-cnt=0
-print('make test...')
-x_test = DataSequence(dataset = 'test', batch_size = BATCH_SIZE, data_augmentation = False, return_target = False)
-print('make valid...')
-x_valid = DataSequence(dataset = 'validation', batch_size = BATCH_SIZE, data_augmentation = False, return_target = True)
-# x_ = DataSequence(dataset = 'validation', batch_size = BATCH_SIZE, data_augmentation = False, return_target = False)
 
-model_list = [x for x in os.listdir(MODEL_DIR) if 'h5' in x]
-print(model_list)  
+def main() -> None:
+    predict()
 
-for file_path in model_list:
-	model = load_model(os.path.join(MODEL_DIR, file_path))
-	print('for load model '+str(cnt))	
-	print('test predict...')
-	predict_test(cnt,model,x_test)
-	print('valid predict.')
-	predict_valid(cnt,model,x_valid)
-	cnt+=1
+
+if __name__ == '__main__':
+    main()
